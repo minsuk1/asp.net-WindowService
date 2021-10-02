68a9dcda5d75d0b85fcf44db65f6c89419c36593
minsuk <palt1230@naver.com>
2021-10-02 20:25:53 +0900

    init

---
+++ WindowsService_HostAPI/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WindowsService_HostAPI
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public String GetString(Int32 id)
+        {
+            return "This is string returned through the Windows service.";
+        }
+
+        [HttpPost]
+        public object PostData([FromBody] object data)
+        {
+            using (var client = new HttpClient())
+            {
+                var json = new JObject();
+                json.Add("id", "Luna");
+                json.Add("name", "Silver");
+                json.Add("age", 19);
+                //Postman에서
+                //http://localhost:8080/Values/PostData/3
+                // {
+                //        "productId":"second",
+                //        "qty":2,
+                //        "unitPrice":2
+                //    }
+                client.BaseAddress = new Uri("http://localhost:3000");
+                var response = client.PostAsJsonAsync("api/products", json).Result;
+            }
+
+            return data;
+        }
+        // http://localhost:8080/Values/PostData/3
+    }
+}
